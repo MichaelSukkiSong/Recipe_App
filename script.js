bd5d1a9084ae674af75e616d185a46074074ff8f
sa77irak <sa77irak@gmail.com>
2020-11-25 16:44:46 +0900

    added the search recipe feature

--- script.js
+++ script.js
@@ -8,10 +8,12 @@
 
 const elements = {
     randomImage: document.getElementById("random-recipe-img"),
-    randomName: document.querySelector(".random-recipe-footer h3")
+    randomName: document.querySelector(".random-recipe-footer h3"),
+    recipeSection: document.querySelector(".random-recipe-section"),
+    inputField: document.querySelector(".search-field"),
 }
 
-/* random recipe section */
+/* functions */
 
 const getRandomRecipe = async () => {
     // fetch data
@@ -26,13 +28,61 @@ const getRandomRecipe = async () => {
     elements.randomName.textContent = sttMeal;
 }
 
-getRandomRecipe();
-
 const searchMeal = async (query) => {
-    // fetch data
-    const result = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)
-    const data = await result.json();
-    console.log(data.meals[0]);
+    try {
+        // fetch data
+        const result = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)
+        const data = await result.json();
+        // console.log(data.meals);
+        return data.meals;
+    } catch (error) {
+        alert(error);
+    }
 }
 
-searchMeal('pizza');
+const displaySearch = (data) => {
+    let new_markup = '';
+    for (let i = 0; i < data.length; i++ ) {
+        const markup = `
+            <div class="recipe-card">
+                <div class="random-recipe-img-container">
+                    <a href="#" >
+                        <img src=${data[i].strMealThumb} id="random-recipe-img">
+                    </a>
+                </div>
+                <div class="random-recipe-footer">
+                    <h3>${data[i].strMeal}</h3>
+                    <button class="like-btn"><i class="far fa-heart"></i></button>
+                </div>
+            </div>
+            `;
+        new_markup += markup;
+    }
+    elements.recipeSection.insertAdjacentHTML("beforeend", new_markup);
+}
+
+const clearSearchResult = () => {
+    elements.recipeSection.innerHTML = '';
+}
+
+/* search button */
+
+document.querySelector(".search-form").addEventListener("submit",async (e) => {
+    e.preventDefault();
+    // get query from input
+    const input = elements.inputField.value;
+
+    // search the query
+    const searchResult = await searchMeal(input);
+    console.log(searchResult) // [{}, {}, {}, ...]
+
+    // clear search results
+    clearSearchResult();
+
+    // display in UI
+    displaySearch(searchResult);
+})
+
+/* on load */
+
+window.addEventListener("load", getRandomRecipe)
