b6f5f0338bd83c45e8d9d9d690a09aae7a1d072e
sa77irak <sa77irak@gmail.com>
2020-11-26 16:08:14 +0900

    added favorites feature using local storage

--- script.js
+++ script.js
@@ -13,6 +13,10 @@ const elements = {
     inputField: document.querySelector(".search-field"),
     recipeCard: document.querySelector(".recipe-card"),
     likeBtn: document.querySelector(".like-btn"),
+    favorites: document.querySelector(".favorites"),
+    favorite: document.querySelector(".favorite"),
+    favoriteImg: document.querySelector(".favorite > a > img"),
+    favoriteName: document.querySelector(".favorite-text"),
 }
 
 /* functions */
@@ -66,8 +70,64 @@ const displaySearch = (data) => {
     elements.recipeSection.insertAdjacentHTML("beforeend", new_markup);
 }
 
+
+const getLocalStorageID = () => {
+    const items = {...localStorage};
+    const itemsIDArr = Object.keys(items);
+    return itemsIDArr;
+}
+
+const getRecipeDetailByID = async (id) => {
+    try {
+        const result = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+        const data = await result.json();
+        // console.log(data.meals[0]);
+        return data.meals[0];
+    } catch(error) {
+        alert(error);
+    }
+}
+
+const displayFavorite = (data) => {
+    let new_markup = '';
+    for (let i = 0; i < data.length; i++) {
+        const markup = `
+        <div class="favorite">
+            <a href="#" >
+                <img src=${data[i].strMealThumb} alt="favorite-food-image"></br>
+                <span class="favorite-text">${data[i].strMeal}</span>
+            </a>
+        </div>
+        `;
+        new_markup += markup;
+    }
+    elements.favorites.insertAdjacentHTML("beforeend", new_markup);
+}
+
+const loadLocalStorageDBToUI = () => {
+        // get local storage id data
+        const itemsIDArr = getLocalStorageID();
+        // console.log(itemsIDArr);
+
+        // get detailed meal data
+        const mealDetailsArr = itemsIDArr.map(el => getRecipeDetailByID(el));
+        // console.log(mealDetailsArr);
+        
+        // clear favorites UI
+        clearFavorites();
+
+        // load image and name in favorites UI
+        Promise.all(mealDetailsArr).then(val => {
+            displayFavorite(val);
+        })
+}
+
 const clearSearchResult = () => {
     elements.recipeSection.innerHTML = '';
+}
+
+const clearFavorites = () => {
+    elements.favorites.innerHTML = '';
 }
 
 function toggleSearchedRecipeFavFunc(x) {
@@ -77,19 +137,10 @@ function toggleSearchedRecipeFavFunc(x) {
     // add recipe id to favorite meals(UI)
 }
 
-const controlFavorite = () => {
-    // toggle favorite icon
-    console.log(elements.likeBtn.firstElementChild);
-    elements.likeBtn.firstElementChild.classList.toggle("fas");
-
-    // get recipe id
-    // add recipe id to favorite meals(DB)
-    // add recipe id to favorite meals(UI)
-}
-
 /* on load */
 
-window.addEventListener("load", getRandomRecipe)
+window.addEventListener("load", getRandomRecipe);
+window.addEventListener("load", loadLocalStorageDBToUI);
 
 /* search button */
 
@@ -111,11 +162,19 @@ document.querySelector(".search-form").addEventListener("submit", async (e) => {
 
 /* favorite button */
 
-// random recipe page
 elements.recipeSection.addEventListener("click", e => {
     if (e.target.matches(".like-btn, .like-btn *")) {
-        controlFavorite();
+        // toggle icon
+        elements.likeBtn.firstElementChild.classList.toggle("fas");
+
+        // get recipe id
+        const randomRecipeID = e.target.parentNode.parentNode.parentNode.id
+        // console.log(randomRecipeID);
+        
+        // add recipe id to favorites localStorage DB
+        localStorage.setItem(randomRecipeID, randomRecipeID);
+
+        loadLocalStorageDBToUI();
     }
 })
 
-// TODO : 
